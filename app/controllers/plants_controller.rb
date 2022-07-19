48e137f69407b08e1890511d394607219dff8264
Kaegan O'Hara <kohara321@gmail.com>
2022-07-19 10:34:47 -0600

    commit tree f66e91e54f2dc6ee462b0bc4ca6f7f2701d61b23

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,33 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      render_not_found
+    end
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find_by(id: params[:id])
+  end
+
+  def render_not_found
+    render json: { error: "Plant not found" }, status: :not_found 
   end
 end
